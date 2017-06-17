e71d2898706f382ebb5ec66cfb1e67ed4e24873b
Seon-Wook Park <seon.wook@swook.net>
2017-06-17 13:17:13 +0200

    Working device assignment

--- optimization/optimize_device_assignment.py
+++ optimization/optimize_device_assignment.py
@@ -1,4 +1,4 @@
-import gurobipy as grb
+from gurobipy import *
 import numpy as np
 
 
@@ -15,5 +15,53 @@ Output:
         'watch': [Element1],
     }
 '''
-def optimize(elements):
-    pass
+def optimize(elements, devices):
+    n = len(elements)
+    m = len(devices)
+
+    # Create empty model
+    model = Model('device_assignment')
+
+    # Add decision variables
+    x = {}
+    for e in elements:
+        for d in devices:
+            x[(e, d)] = model.addVar(vtype=GRB.BINARY, name='%s_%s' % (e.name, d.name))
+    model.update()
+
+    # Constraint 1: sum of element sizes don't exceed device class capacity
+    for d in devices:
+        model.addConstr(quicksum(e.size * x[(e, d)] for e in elements) <= d.capacity,
+                        'capacity_constraint_%s' % d.name)
+
+    # Constraint 2: element should be somewhere
+    for e in elements:
+        model.addConstr(quicksum(x[(e, d)] for d in devices) >= 1,
+                        'element_is_assigned_constraint_%s' % e.name)
+
+    # Constraint 3: a device class should have at least one element assigned
+    for d in devices:
+        model.addConstr(quicksum(x[(e, d)] for e in elements) >= 1,
+                        'device_has_some_elements_constraint_%s' % d.name)
+    model.update()
+
+    # Objective function
+    cost = quicksum(e.importance * x[(e, d)]
+                    for e in elements
+                        for d in devices)
+    model.setObjective(cost, GRB.MAXIMIZE)
+
+    # Solve
+    model.optimize()
+
+    # Create output list of elements (sorted)
+    output = {}
+    for key, var in x.items():
+        if var.x != 1:  # Ignore if not 1.0 (assignment)
+            continue
+        element, device = key
+        if device.name not in output:
+            output[device.name] = []
+        output[device.name].append(element)
+
+    return output
--- optimization/test_device_assignment.py
+++ optimization/test_device_assignment.py
@@ -9,18 +9,21 @@ elements = [
     Element(name='play',  type='', size=1, importance=9),
     Element(name='next',  type='', size=1, importance=2),
     Element(name='prev',  type='', size=1, importance=2),
+    Element(name='comments', type='', size=3, importance=5),
 ]
-random.shuffle(elements)
+# random.shuffle(elements)
 
 devices = [
-    Device(name='tv', capacity=6),
-    Device(name='pc', capacity=4),
-    Device(name='tablet', capacity=3),
-    Device(name='phone', capacity=2)
+    Device(name='tv', capacity=15),
+    Device(name='pc', capacity=8),
+    Device(name='tablet', capacity=4),
+    Device(name='phone', capacity=2),
+    Device(name='watch', capacity=1),
 ]
 
 
 output = optimize_device_assignment.optimize(elements, devices)
 
-print('inputs: %s' % [e.name for e in elements])
-print('outputs: %s' % [e.name for e in output])
+print('\ninputs: %s' % [e.name for e in elements])
+for key, elements in output.items():
+    print('outputs[%s]: %s' % (key, [e.name for e in elements]))
