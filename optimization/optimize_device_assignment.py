8ebcdb6c6f4ff9904427e8e452471976dd32a90d
unknown <cgebhard@ohws08.d.ethz.ch>
2017-07-10 19:20:18 +0200

    - adjusted optimization method based on new formulation - implemented preprocessing function for object lists

--- optimization/optimize_device_assignment.py
+++ optimization/optimize_device_assignment.py
@@ -2,10 +2,6 @@ from gurobipy import *
 import numpy as np
 
 
-def preprocess(elements, devices, users):
-    pass
-
-
 '''
 
 Input:
@@ -22,39 +18,50 @@ Output:
     }
 '''
 def optimize(elements, devices, users):
-    n = len(elements)
-    m = len(devices)
+    [widget_element_imp, element_user_imp, element_widget_device_comp, user_device_acc,
+     widget_element_size, num_users, num_devices, num_elements, num_widgets] = pre_process(elements, devices, users)
 
     # Create empty model
     model = Model('device_assignment')
 
     # Add decision variables
     x = {}
-    for e in elements:
-        for d in devices:
-            x[(e, d)] = model.addVar(vtype=GRB.BINARY, name='%s_%s' % (e.name, d.name))
+    for w in range(0, num_widgets):
+        for e in range(0, num_elements):
+            for d in (0, num_devices):
+                x[(w, e, d)] = model.addVar(vtype=GRB.BINARY, name='%i_%s_%s' % (w, elements[e].name, devices[d].name))
     model.update()
 
-    # Constraint 1: sum of element sizes don't exceed device class capacity
-    for d in devices:
-        model.addConstr(quicksum(e.size * x[(e, d)] for e in elements) <= d.capacity,
-                        'capacity_constraint_%s' % d.name)
+    for d in range(0, num_devices):
+        # Constraint 1: sum of widgets shouldn't exceed device class capacity
+        model.addConstr(quicksum(widget_element_size[w, e] * x[(w, e, d)]
+                                 for w in range(0, num_widgets)
+                                    for e in range(0, num_elements)) <= devices[d].capacity,
+                            'capacity_constraint_%s' % devices[d].name)
 
-    # Constraint 2: a device class should have at least one element assigned
-    for d in devices:
-        model.addConstr(quicksum(x[(e, d)] for e in elements) >= 1,
-                        'device_has_some_elements_constraint_%s' % d.name)
+        if np.any(user_device_acc[:, d]):
+            # Constraint 2: a device which is accessible by a user should have at least one widget
+            model.addConstr(quicksum(x[(w, e, d)] for w in range(0, num_widgets)
+                                                    for e in range(0, num_elements)) >= 1,
+                            'at_least_one_widget_constraint_%s' % devices[d].name)
+        else:
+            # Constraint 3: a device which is not accessible by any user should not have a widget
+            model.addConstr(quicksum(x[(w, e, d)] for w in range(0, num_widgets)
+                                     for e in range(0, num_elements)) == 0,
+                            'no_widget_constraint_%s' % devices[d].name)
 
-    # Constraint 3: show element at least once
-    for e in elements:
-        model.addConstr(quicksum(x[(e, d)] for d in devices) >= 1,
-                        'element_at_least_once_constraint_%s' % e.name)
+        for e in range(0, num_elements):
+            # Constraint 4: one device should not contain multiple representations of one UI element
+            model.addConstr(quicksum(x[(w, e, d)] for w in range(0, num_widgets)) <= 1,
+                            'one_element_widget_per_device_constraint_%s_%s' % (elements[e].name, devices[d].name))
+
     model.update()
-
     # Objective function
-    cost = quicksum(e.importance * x[(w, e, d)]
-                    for e in elements
-                        for d in devices)
+    cost = quicksum(widget_element_imp[w, e] * element_user_imp[e, u] * element_widget_device_comp[e, w, d] * user_device_acc[u, d] * x[(w, e, d)]
+                    for w in range(0, num_widgets)
+                        for e in range(0, num_elements)
+                            for d in range(0, num_devices)
+                                for u in range(0, num_users))
     model.setObjective(cost, GRB.MAXIMIZE)
 
     # Solve
@@ -73,3 +80,31 @@ def optimize(elements, devices, users):
         output[device.name].append(element)
 
     return output
+
+
+def pre_process_objects(elements, devices, users):
+    num_elements = len(elements)
+    num_devices = len(devices)
+    num_users = len(users)
+    num_widgets = 3
+
+    user_device_acc = np.zeros((num_users, num_devices))
+    for d in range(num_devices):
+        for user in devices[d].users:
+            user_device_acc[users.index(user), d] = 1
+
+    widget_element_imp = np.ones((num_widgets, num_elements)) * -1
+    widget_element_size = np.zeros((num_widgets, num_elements))
+    element_widget_device_comp = np.zeros((num_elements, num_widgets, num_devices))
+    for e in range(num_elements):
+        w = 0
+        for widget in elements[e].widgets:
+            widget_element_imp[w, e] = w
+            widget_element_size[w, e] = widget.size
+            for d in range(num_devices):
+                element_widget_device_comp[e, w, d] = devices[d].calculate_compatibility(widget)
+            w += 1
+
+    element_user_imp = np.ones((num_elements, num_users)) #define importance per element and user!
+    return [widget_element_imp, element_user_imp, element_widget_device_comp, user_device_acc,
+     widget_element_size, num_users, num_devices, num_elements, num_widgets]
