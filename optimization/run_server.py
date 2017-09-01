e5ff5c3733f84aa45deac2d627324cc2870cda63
Seonwook Park <spark@inf.ethz.ch>
2017-09-01 11:02:28 +0200

    Better handling of messages in backend interface

--- optimization/run_server.py
+++ optimization/run_server.py
@@ -1,33 +1,45 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+import logging
 import optimize
 import json
 import traceback
 
+logger = logging.getLogger('SoManyScreens_backend')
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+
 class SimpleEcho(WebSocket):
-    recv = ""
-    send = ""
+
     def optimize(self):
-        if self.data == '{"type":"alive"}':
+        # Handle keep-alive
+        try:
+            json_request = json.loads(self.data)
+        except:
             pass
+        if 'type' in json_request and json_request['type'] == 'alive':
+            return
+
+        # Handle proper input
         try:
             web_output = optimize.handle_web_input(self.data)
             self.sendMessage(web_output)
         except:
             tb = traceback.format_exc()
-            print('\n%s\n' % tb)
+            logger.debug('\n%s\n' % tb)
             self.sendMessage('{"error": "%s"}' % tb)
 
     def handleMessage(self):
-        print(self.data)
+        logger.debug('\nReceived data from client:\n%s\n' % self.data)
         self.optimize()
 
     def handleConnected(self):
-        print(self.address, 'connected')
+        logger.info('%s:%d connected.' % self.address)
 
     def handleClose(self):
-        print(self.address, 'closed')
+        logger.info('%s:%d disconnected.' % self.address)
 
 
-
-server = SimpleWebSocketServer('', 8000, SimpleEcho)
+port = 8001
+logger.info('Starting backend at port %d' % port)
+server = SimpleWebSocketServer('', port, SimpleEcho)
 server.serveforever()
