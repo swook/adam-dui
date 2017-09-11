0a9ca853bca4fdfbe32330e4b3dbd09960decd1c
Seonwook Park <spark@inf.ethz.ch>
2017-09-11 10:26:35 +0200

    Add Quaterly Figures

--- scenarios/4_christoph_study.py
+++ scenarios/4_christoph_study.py
@@ -14,6 +14,7 @@ element_definitions = {
     'Notes (Employee4)':       ' 3 | 300 | 500 | 1000 | 1500 | 0530 | Employee4',
     'Notes (Employee5)':       ' 3 | 300 | 500 | 1000 | 1500 | 0530 | Employee5',
     'Clock':                   ' 1 | 100 | 100 |  300 |  300 | 1000',
+    'Quaterly Figures':        ' 1 | 500 | 800 |  900 | 1500 | 5000 | Boss,Assistant',
 }
 # Name | Width | Height | Properties | Users
 # Properties: visual_display, text_input, touch_pointing, mouse_pointing
@@ -68,6 +69,7 @@ scenario.add_elements_from_text(pick(element_definitions,
      'Notes (Employee3)',
      'Notes (Employee4)',
      'Clock',
+     'Quaterly Figures',
      ]))
 scenario.add_devices_from_text(pick(device_definitions,
     ['Projector1',
@@ -88,14 +90,16 @@ scenario.add_devices_from_text(pick(device_definitions,
      'Laptop (Assistant)',
      ]))
 scenario.set_user_importance('Boss', 'Notes (Shared)', 8)
+scenario.set_user_importance('Boss', 'Quaterly Figures', 7)
 scenario.set_user_importance('Assistant', 'Notes (Shared)', 8)
+scenario.set_user_importance('Assistant', 'Quaterly Figures', 7)
 scenario.set_user_importance('Presenter', 'Clock', 6)
 scenario.run(expect={
     'Projector1':         ['Presentation (View)', '~Notes (Shared)'],
     'Projector2':         ['Presentation (View)', '~Notes (Shared)'],
     'Laptop (Presenter)': ['Presentation (Notes)'],
-    'Laptop (Assistant)': ['Notes (Shared)'],
-    'Laptop (Boss)':      ['Notes (Shared)'],
+    'Laptop (Assistant)': ['Notes (Shared)', 'Quaterly Figures'],
+    'Laptop (Boss)':      ['Notes (Shared)', 'Quaterly Figures'],
     'Laptop (Employee1)': ['Notes (Employee1)'],
     'Laptop (Employee2)': ['Notes (Employee2)'],
     'Laptop (Employee3)': ['Notes (Employee3)'],
@@ -122,6 +126,7 @@ scenario.add_elements_from_text(pick(element_definitions,
      'Notes (Employee4)',
      'Notes (Employee5)',
      'Clock',
+     'Quaterly Figures',
      ]))
 scenario.add_devices_from_text(pick(device_definitions,
     ['Projector1',
@@ -146,14 +151,16 @@ scenario.add_devices_from_text(pick(device_definitions,
      'Laptop (Assistant)',
      ]))
 scenario.set_user_importance('Boss', 'Notes (Shared)', 8)
+scenario.set_user_importance('Boss', 'Quaterly Figures', 7)
 scenario.set_user_importance('Assistant', 'Notes (Shared)', 8)
+scenario.set_user_importance('Assistant', 'Quaterly Figures', 7)
 scenario.set_user_importance('Presenter', 'Clock', 6)
 scenario.run(expect={
     'Projector1':         ['Presentation (View)', '~Notes (Shared)'],
     'Projector2':         ['Presentation (View)', '~Notes (Shared)'],
     'Laptop (Presenter)': ['Presentation (Notes)'],
-    'Laptop (Assistant)': ['Notes (Shared)'],
-    'Laptop (Boss)':      ['Notes (Shared)'],
+    'Laptop (Assistant)': ['Notes (Shared)', 'Quaterly Figures'],
+    'Laptop (Boss)':      ['Notes (Shared)', 'Quaterly Figures'],
     'Laptop (Employee1)': ['Notes (Employee1)'],
     'Laptop (Employee2)': ['Notes (Employee2)'],
     'Laptop (Employee3)': ['Notes (Employee3)'],
